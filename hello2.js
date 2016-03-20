d803983b744207e07167282c80d9de0c4a16000c
Eanna <ehego@live.ie>
2016-03-20 12:08:42 +0000

    Comit2

---
+++ hello2.js
@@ -0,0 +1,30 @@
+var helo = function(){console.log("hello, Who are u?")}
+
+
+function myfun(fn){
+    fn();
+    
+}
+
+myfun(helo);
+
+ var i =function(){console.log("Hi, I am Eanna")};
+myfun(i);
+
+myfun(function(){console.log("Hello Eanna, I am Caroline")
+;})
+
+
+
+var obj = {
+    greet:'hello'
+    
+}
+
+console.log(obj.greet);
+
+
+var prop = "greet";
+
+console.log(obj[prop]);
+//doing this for git
